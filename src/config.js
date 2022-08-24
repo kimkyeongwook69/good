8a6784c2811652d1770889aae5db62e7cafcaf68
F91 <plusf91@gmail.com>
2022-08-24 10:54:36 +0900

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const CLIENT_ID = "--------------------------"
+export const CLIENT_ID = "4309352159504a81b378691b675fd78f"
 export const REDIRECT_URI = "http://localhost:3000"
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 export const RESPONSE_TYPE = "token"
